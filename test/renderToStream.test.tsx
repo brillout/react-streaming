8bc7e938e07b2a0241b25919a6381a911bbbeafe
Romuald Brillout <git@brillout.com>
2022-05-12 15:15:01 +0200

    add further tests for disable mode

--- test/renderToStream.test.tsx
+++ test/renderToStream.test.tsx
@@ -14,51 +14,55 @@ describe('renderToStream()', async () => {
     })
   })
   ;(['node', 'web'] as const).forEach((streamType: 'node' | 'web') => {
-    it(`injectToStream - basic - ${streamType} stream`, async () => {
-      const { data, streamEnd, injectToStream } = await render(<>hi</>, { streamType, disable: false })
-      injectToStream('<script type="module" src="/main.js"></script>')
-      await streamEnd
-      expect(data.content).toBe('hi<!-- --><script type="module" src="/main.js"></script>')
+    ;[true, false].forEach((disable) => {
+      it(`injectToStream - basic - ${streamType} stream${disable ? ' - disabled' : ''}`, async () => {
+        const { data, streamEnd, injectToStream } = await render(<>hi</>, { streamType, disable })
+        injectToStream('<script type="module" src="/main.js"></script>')
+        await streamEnd
+        expect(data.content).toBe('hi<!-- --><script type="module" src="/main.js"></script>')
+      })
     })
   })
   ;(['node', 'web'] as const).forEach((streamType: 'node' | 'web') => {
-    it(`injectToStream - useAsync() - ${streamType} stream`, async () => {
-      const { data, streamEnd, injectToStream } = await render(<Page />, { streamType, disable: false })
-      injectToStream('<script type="module" src="/main.js"></script>')
+    ;[true, false].forEach((disable) => {
+      it(`injectToStream - useAsync() - ${streamType} stream${disable ? ' - disabled' : ''}`, async () => {
+        const { data, streamEnd, injectToStream } = await render(<Page />, { streamType, disable: false })
+        injectToStream('<script type="module" src="/main.js"></script>')
 
-      let timeoutResolved = false
-      setTimeout(() => {
-        expect(data.content.endsWith('<script type="module" src="/main.js"></script>')).toBe(true)
-        timeoutResolved = true
-      }, 5)
+        let timeoutResolved = false
+        setTimeout(() => {
+          expect(data.content.endsWith('<script type="module" src="/main.js"></script>')).toBe(true)
+          timeoutResolved = true
+        }, 5)
 
-      expect(timeoutResolved).toBe(false)
-      await streamEnd
-      expect(timeoutResolved).toBe(true)
+        expect(timeoutResolved).toBe(false)
+        await streamEnd
+        expect(timeoutResolved).toBe(true)
 
-      try {
-        injectToStream('someChunk')
-        expect(1).toBe(2)
-      } catch (err) {
-        expect(err.message).toBe(
-          '[react-streaming][Wrong Usage] Cannot inject following chunk after stream has ended: `someChunk`'
+        try {
+          injectToStream('someChunk')
+          expect(1).toBe(2)
+        } catch (err) {
+          expect(err.message).toBe(
+            '[react-streaming][Wrong Usage] Cannot inject following chunk after stream has ended: `someChunk`'
+          )
+        }
+
+        expect(data.content).toBe(
+          [
+            '<h1>Welcome</h1>This page is:<!-- --><ul>',
+            // Suspense fallback
+            '<li><!--$?--><template id="B:0"></template><p>Loading...</p><!--/$--></li>',
+            '<li>Rendered to HTML.</li><li>Interactive. <!-- --><button type="button">Counter <!-- -->0<!-- --></button></li></ul>',
+            // Injection
+            '<script type="module" src="/main.js"></script>',
+            // Injection useAsync()
+            '<script class="react-streaming_ssr-data" type="application/json">[{"key":":R7:","value":"Hello, I was lazy."}]</script>',
+            // Suspense resolving
+            '<div hidden id="S:0"><p>Hello, I was lazy.</p></div><script>function $RC(a,b){a=document.getElementById(a);b=document.getElementById(b);b.parentNode.removeChild(b);if(a){a=a.previousSibling;var f=a.parentNode,c=a.nextSibling,e=0;do{if(c&&8===c.nodeType){var d=c.data;if("/$"===d)if(0===e)break;else e--;else"$"!==d&&"$?"!==d&&"$!"!==d||e++}d=c.nextSibling;f.removeChild(c);c=d}while(c);for(;b.firstChild;)f.insertBefore(b.firstChild,c);a.data="$";a._reactRetry&&a._reactRetry()}};$RC("B:0","S:0")</script>'
+          ].join('')
         )
-      }
-
-      expect(data.content).toBe(
-        [
-          '<h1>Welcome</h1>This page is:<!-- --><ul>',
-          // Suspense fallback
-          '<li><!--$?--><template id="B:0"></template><p>Loading...</p><!--/$--></li>',
-          '<li>Rendered to HTML.</li><li>Interactive. <!-- --><button type="button">Counter <!-- -->0<!-- --></button></li></ul>',
-          // Injection
-          '<script type="module" src="/main.js"></script>',
-          // Injection useAsync()
-          '<script class="react-streaming_ssr-data" type="application/json">[{"key":":R7:","value":"Hello, I was lazy."}]</script>',
-          // Suspense resolving
-          '<div hidden id="S:0"><p>Hello, I was lazy.</p></div><script>function $RC(a,b){a=document.getElementById(a);b=document.getElementById(b);b.parentNode.removeChild(b);if(a){a=a.previousSibling;var f=a.parentNode,c=a.nextSibling,e=0;do{if(c&&8===c.nodeType){var d=c.data;if("/$"===d)if(0===e)break;else e--;else"$"!==d&&"$?"!==d&&"$!"!==d||e++}d=c.nextSibling;f.removeChild(c);c=d}while(c);for(;b.firstChild;)f.insertBefore(b.firstChild,c);a.data="$";a._reactRetry&&a._reactRetry()}};$RC("B:0","S:0")</script>'
-        ].join('')
-      )
+      })
     })
   })
 })
