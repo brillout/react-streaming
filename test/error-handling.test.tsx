33d782bf8dd8b38c0e852f4d1a41a3f990920e80
Romuald Brillout <git@brillout.com>
2022-08-29 18:02:58 +0200

    update react

--- test/error-handling.test.tsx
+++ test/error-handling.test.tsx
@@ -3,6 +3,7 @@ import { render } from './render'
 import { onConsoleError } from './onConsoleError'
 import React, { Suspense } from 'react'
 import { useAsync } from '../src/index'
+import { partRegex } from '@brillout/part-regex'
 
 describe('error handling', async () => {
   ;(['node', 'web'] as const).forEach((streamType: 'node' | 'web') => {
@@ -74,16 +75,30 @@ describe('error handling', async () => {
       // React swallows the error, and retries to resolve the suspense on the cient-side
       const { data, streamEnd } = await render(<Page />, { streamType, onBoundaryError })
       await streamEnd
-      expect(data.content).toBe(
-        [
+      {
+        const filePointer = /[^\)]+/
+        const split = '<script>'
+        const [dataBegin, dataEnd, ...rest] = data.content.split(split)
+        expect(rest.length).toBe(0)
+        const dataContentExpected = [
           // Page Shell
           '<!--$?--><template id="B:0"></template><p>Loading...</p><!--/$-->',
           // `useAsync()` script injection
           '<script class="react-streaming_ssr-data" type="application/json">{"key":":R0:","value":"Hello, I was lazy.","deps":[]}</script>',
-          // React telling the client-side to re-try
-          '<script>function $RX(a){if(a=document.getElementById(a))a=a.previousSibling,a.data="$!",a._reactRetry&&a._reactRetry()};$RX("B:0")</script>'
         ].join('')
-      )
+        try {
+          expect(dataBegin).toMatch(dataContentExpected)
+        } catch (err) {
+          console.log('expected:', dataContentExpected)
+          console.log('actual:', data.content)
+          console.log('\n\n\n')
+          throw err
+        }
+        // React handling the suspense boundary error
+        expect(split + dataEnd).toMatch(
+          partRegex`<script>function $RX(b,c,d,e){var a=document.getElementById(b);a&&(b=a.previousSibling,b.data="$!",a=a.dataset,c&&(a.dgst=c),d&&(a.msg=d),e&&(a.stck=e),b._reactRetry&&b._reactRetry())};$RX("B:0","","some-error","\\n    at Page (${filePointer})\\n    at SsrDataProvider (${filePointer})")</script>`
+        )
+      }
       expect(onBoundaryError).toBeCalled()
     })
   })
--- test/renderToStream.test.tsx
+++ test/renderToStream.test.tsx
@@ -19,7 +19,7 @@ describe('renderToStream()', async () => {
         const { data, streamEnd, injectToStream } = await render(<>hi</>, { streamType, disable })
         injectToStream('<script type="module" src="/main.js"></script>')
         await streamEnd
-        expect(data.content).toBe('hi<!-- --><script type="module" src="/main.js"></script>')
+        expect(data.content).toBe('hi<script type="module" src="/main.js"></script>')
       })
     })
   })
@@ -48,12 +48,12 @@ describe('renderToStream()', async () => {
           )
         }
 
-        expect(data.content).toBe(
-          [
-            '<h1>Welcome</h1>This page is:<!-- --><ul>',
+        {
+          const dataContent = [
+            '<h1>Welcome</h1>This page is:<ul>',
             // Suspense fallback
             '<li><!--$?--><template id="B:0"></template><p>Loading...</p><!--/$--></li>',
-            '<li>Rendered to HTML.</li><li>Interactive. <!-- --><button type="button">Counter <!-- -->0<!-- --></button></li></ul>',
+            '<li>Rendered to HTML.</li><li>Interactive. <button type="button">Counter <!-- -->0</button></li></ul>',
             // Injection
             '<script type="module" src="/main.js"></script>',
             // Injection useAsync()
@@ -61,7 +61,15 @@ describe('renderToStream()', async () => {
             // Suspense resolving
             '<div hidden id="S:0"><p>Hello, I was lazy.</p></div><script>function $RC(a,b){a=document.getElementById(a);b=document.getElementById(b);b.parentNode.removeChild(b);if(a){a=a.previousSibling;var f=a.parentNode,c=a.nextSibling,e=0;do{if(c&&8===c.nodeType){var d=c.data;if("/$"===d)if(0===e)break;else e--;else"$"!==d&&"$?"!==d&&"$!"!==d||e++}d=c.nextSibling;f.removeChild(c);c=d}while(c);for(;b.firstChild;)f.insertBefore(b.firstChild,c);a.data="$";a._reactRetry&&a._reactRetry()}};$RC("B:0","S:0")</script>'
           ].join('')
-        )
+          try {
+            expect(data.content).toBe(dataContent)
+          } catch (err) {
+            console.log('expected:', dataContent)
+            console.log('actual:', data.content)
+            console.log()
+            throw err
+          }
+        }
       })
     })
   })
