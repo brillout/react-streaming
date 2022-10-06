0afba5768090783271ee725f94c801f78992450a
Romuald Brillout <git@brillout.com>
2022-10-06 14:50:13 +0200

    fix: improve DX upon wrong `useAsync()` usage

--- src/shared/key.ts
+++ src/shared/key.ts
@@ -2,6 +2,7 @@ export { stringifyKey }
 export { assertKey }
 
 import { stringify } from '@brillout/json-serializer/stringify'
+import { isCallable } from '../utils/isCallable'
 import { assertUsage } from './utils'
 
 function stringifyKey(key: unknown): string {
@@ -12,6 +13,10 @@ function stringifyKey(key: unknown): string {
 function assertKey(keyValue: unknown) {
   assertUsage(
     keyValue,
-    `[useAsync(key, asyncFc)] You provided a \`key\` with the value \`${keyValue}\` which is forbidden.`
+    `[useAsync(key, asyncFn)] You provided a \`key\` with the value \`${keyValue}\` which is forbidden.`
+  )
+  assertUsage(
+    !isCallable(keyValue),
+    `[useAsync(key, asyncFn)] You provided a \`key\` that is a function which is forbidden.`
   )
 }
