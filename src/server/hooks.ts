794dd95fa7332d30deba03e1bc66b1c7a40d8a38
Romuald Brillout <git@brillout.com>
2023-06-02 11:11:01 +0200

    fix: improve assertion that server files aren't loaded in browser (fix #22)

--- src/server/hooks.ts
+++ src/server/hooks.ts
@@ -1,2 +1,13 @@
-export { useAsync } from './useAsync'
-export { useStream } from './useStream'
+export { useAsync }
+export { useStream }
+
+// We don't import from ./utils.ts because utils/debug.js contains a !isBrowser() assertion
+import { assertUsage } from '../utils/assert'
+import { isBrowser } from '../utils/isBrowser'
+assertUsage(
+  !isBrowser(),
+  "The file node_modules/react-streaming/dist/{esm,cjs}/server/hooks.js is loaded in the browser but it shouldn't. Make sure to never `import { something } from 'react-streaming/server'` in code that runs on the client-side. Also make sure your bundler picks the right node_modules/react-streaming/package.json#exports entries."
+)
+
+import { useAsync } from './useAsync'
+import { useStream } from './useStream'
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,13 @@
-export { renderToStream, disable } from './renderToStream'
-export type { InjectToStream } from './renderToStream/createBuffer'
+export { renderToStream, disable }
+export type { InjectToStream }
+
+// We don't import from ./utils.ts because utils/debug.js contains a !isBrowser() assertion
+import { assertUsage } from '../utils/assert'
+import { isBrowser } from '../utils/isBrowser'
+assertUsage(
+  !isBrowser(),
+  "The file node_modules/react-streaming/dist/{esm,cjs}/server/index.js is loaded in the browser but it shouldn't. Make sure to never `import { something } from 'react-streaming/server'` in code that runs on the client-side. Also make sure your bundler picks the right node_modules/react-streaming/package.json#exports entries."
+)
+
+import { renderToStream, disable } from './renderToStream'
+import type { InjectToStream } from './renderToStream/createBuffer'
