854011bacc80e1d5aefca3e62b22a1ca435b8b75
Romuald Brillout <git@brillout.com>
2023-10-16 10:16:21 +0200

    fix: make react-streaming/server browser entry a poison pill

---
+++ src/server/client-poison-pill.ts
@@ -0,0 +1,6 @@
+import { assertUsage } from './utils'
+
+assertUsage(
+  false,
+  "`import { something } from 'react-streaming/server'` in client-side code is forbidden: the module react-streaming/server should never be loaded on the client-side"
+)
