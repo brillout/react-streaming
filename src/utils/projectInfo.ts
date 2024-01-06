d4293c7f295673f55da0d25f921775b17e165c5f
Romuald Brillout <git@brillout.com>
2024-01-06 15:31:10 +0100

    fix: ensure single version

--- src/utils/projectInfo.ts
+++ src/utils/projectInfo.ts
@@ -1,8 +1,25 @@
+export { projectInfo }
+
+import { getGlobalObject } from './getGlobalObject'
+
 const PROJECT_VERSION = '0.3.18'
 
-export const projectInfo = {
+const projectInfo = {
   projectName: 'react-streaming' as const,
   projectVersion: PROJECT_VERSION,
   npmPackageName: 'react-streaming' as const,
   githubRepository: 'https://github.com/brillout/react-streaming' as const
+}
+
+const { versions } = getGlobalObject('projectInfo.ts', {
+  versions: new Set<string>()
+})
+versions.add(projectInfo.projectVersion)
+if (versions.size >= 2) {
+  const versionsStr = Array.from(versions)
+    .map((v) => `${projectInfo.projectName}@${v}`)
+    .join('and')
+  throw new Error(
+    `Using different versions is forbidden, but ${versionsStr} are loaded. Make sure only one version is loaded.`
+  )
 }
