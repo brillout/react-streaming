1766596b3ce93920926048e531f660d05b30db71
Romuald Brillout <git@brillout.com>
2024-04-17 17:19:11 +0200

    comment

--- src/utils/isVikeReactApp.ts
+++ src/utils/isVikeReactApp.ts
@@ -1,5 +1,5 @@
 export function isVikeReactApp(): boolean {
   const g = globalThis as Record<string, unknown>
-  // Set by vike-react https://github.com/vikejs/vike-react/blob/ca46452992a7ea94e8f58e40f5062ba56a0b56eb/packages/vike-react/src/renderer/onRenderHtml.tsx#L75
+ // Set by vike-react https://github.com/vikejs/vike-react/blob/23e92434424f10e7e742b6bf587edee5aa8832df/packages/vike-react/src/renderer/onRenderHtml.tsx#L75
   return !!g._isVikeReactApp
 }
