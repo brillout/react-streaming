f33ed72a043f38df90093288ceea687e5ecc9657
Romuald Brillout <git@brillout.com>
2024-06-28 00:14:22 +0200

    fix: fix result type

--- src/server/renderToStream.ts
+++ src/server/renderToStream.ts
@@ -19,6 +19,7 @@ import { assert, assertUsage, getGlobalObject } from './utils'
 import type { renderToNodeStream as renderToNodeStream_ } from './renderToStream/renderToNodeStream'
 import type { renderToWebStream as renderToWebStream_ } from './renderToStream/renderToWebStream'
 import { debugFlow } from './renderToStream/common'
+import type { InjectToStream } from './index.node-and-web'
 const globalObject = getGlobalObject('renderToStream.ts', {
   renderToNodeStream: null as null | typeof renderToNodeStream_,
   renderToWebStream: null as null | typeof renderToWebStream_,
@@ -58,7 +59,7 @@ type Result = (
 ) & {
   streamEnd: Promise<boolean>
   disabled: boolean
-  injectToStream: (chunk: unknown) => void
+  injectToStream: InjectToStream
   abort: () => void
 }
 
