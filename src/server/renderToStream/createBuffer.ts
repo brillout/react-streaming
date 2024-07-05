95c5f6501efaf8831a6def9450f953281f23fde9
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-07-05 03:55:36 +0200

    simplify

--- src/server/renderToStream/createBuffer.ts
+++ src/server/renderToStream/createBuffer.ts
@@ -8,18 +8,12 @@ import { assert, assertUsage, createDebugger, isPromise } from '../utils'
 
 const debug = createDebugger('react-streaming:buffer')
 
-// =================================
-// =================================
-// Potentially much simpler implementation: https://gist.github.com/nitedani/b057ff380fd494dfbd088719925ac077
-// =================================
-// =================================
-
 type InjectToStreamOptions = {
   flush?: boolean
 }
 type Chunk = string | Promise<string> // A chunk doesn't have to be a string. Let's progressively add all expected types as users complain.
 // General notes about how to inject to the stream: https://github.com/brillout/react-streaming/tree/main/src#readme
-type InjectToStream = (chunk: Chunk, options?: InjectToStreamOptions) => Promise<void>
+type InjectToStream = (chunk: Chunk, options?: InjectToStreamOptions) => void
 
 type StreamOperations = {
   operations: null | { writeChunk: (chunk: unknown) => void; flush: null | (() => void) }
@@ -34,16 +28,13 @@ function createBuffer(
   onBeforeEnd: () => Promise<void>
   hasStreamEnded: () => boolean
 } {
-  const buffer: { chunk: Chunk; flush: undefined | boolean }[] = []
-  let bufferIsFlushing: false | Promise<void> = false
   let state: 'UNSTARTED' | 'STREAMING' | 'ENDED' = 'UNSTARTED'
-
-  // See Rule 2: https://github.com/brillout/react-streaming/tree/main/src#rule-2
-  let writePermission = false
+  let userChunkPromise: null | Promise<void> = null
+  let onHead = () => {}
 
   return { injectToStream, onReactWrite, onBeforeEnd, hasStreamEnded }
 
-  async function injectToStream(chunk: Chunk, options?: InjectToStreamOptions) {
+  function injectToStream(chunk: Chunk, options?: InjectToStreamOptions) {
     if (debug.isEnabled) {
       debug('injectToStream()', getChunkAsString(chunk))
     }
@@ -55,66 +46,56 @@ function createBuffer(
         )}`,
       )
     }
-    buffer.push({ chunk, flush: options?.flush })
-    await flushBuffer()
+    const userChunkPromisePrevious = userChunkPromise
+    userChunkPromise = (async () => {
+      if (userChunkPromisePrevious) await userChunkPromisePrevious
+      if (state !== 'STREAMING')
+        await new Promise<void>((resolve) => {
+          onHead = resolve
+        })
+      if (isPromise(chunk)) chunk = await chunk
+      writeChunk(chunk, options?.flush)
+    })()
   }
 
-  async function flushBuffer() {
-    if (!writePermission) {
-      return
-    }
-    if (buffer.length === 0) {
-      return
-    }
-    if (bufferIsFlushing) {
-      await bufferIsFlushing
-      return
-    }
+  function writeChunk(chunk: unknown, flush?: boolean) {
     if (state !== 'STREAMING') {
       assert(state === 'UNSTARTED')
       return
     }
-    let flushStream = false
-    let resolve: () => void
-    bufferIsFlushing = new Promise((r) => (resolve = r))
-    while (buffer.length > 0) {
-      let { chunk, flush } = buffer.shift()!
-      assert(streamOperations.operations)
-      const { writeChunk } = streamOperations.operations
-      if (isPromise(chunk)) chunk = await chunk
-      writeChunk(chunk)
-      if (flush) flushStream = true
-    }
-    bufferIsFlushing = false
-    resolve!()
-    assert(buffer.length === 0)
     assert(streamOperations.operations)
-    if (flushStream && streamOperations.operations.flush !== null) {
+    streamOperations.operations.writeChunk(chunk)
+    if (flush && streamOperations.operations.flush !== null) {
       streamOperations.operations.flush()
       debug('stream flushed')
     }
   }
 
   async function onReactWrite(chunk: unknown) {
-    state === 'UNSTARTED' && debug('>>> START')
+    const firstWrite = state === 'UNSTARTED'
+    firstWrite && debug('>>> START')
     if (debug.isEnabled) {
       debug('react write', getChunkAsString(chunk))
     }
-    state = 'STREAMING'
-    const bufferReactEntry = { chunk: chunk as any, flush: true }
-    if (!writePermission) {
-      buffer.unshift(bufferReactEntry)
+
+    // See Rule 2: https://github.com/brillout/react-streaming/tree/main/src#rule-2
+    if (firstWrite) {
+      state = 'STREAMING'
+      onHead()
     } else {
-      buffer.push(bufferReactEntry)
+      await userChunkPromise
     }
-    writePermission = true
-    await flushBuffer()
+
+    writeChunk(chunk, true)
   }
 
   async function onBeforeEnd() {
-    writePermission = true // in case React didn't write anything
-    await flushBuffer()
-    assert(buffer.length === 0)
+    {
+      // in case React didn't write anything
+      state = 'STREAMING'
+      onHead()
+    }
+    await userChunkPromise
     await doNotClosePromise.promise
     state = 'ENDED'
     debug('>>> END')
