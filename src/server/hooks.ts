34b5d0e01e91dcd83845edd8f95fc416548326dd
Romuald Brillout <git@brillout.com>
2025-02-16 15:06:01 +0100

    minor refactor

--- src/server/hooks.ts
+++ src/server/hooks.ts
@@ -1,5 +1,5 @@
-export { useAsync }
-export { useStream }
+export { useAsync } from './useAsync'
+export { useStream } from './useStream'
 
 // We don't import from ./utils.ts because utils/debug.js contains a !isBrowser() assertion
 import { assertUsage } from '../utils/assert'
@@ -8,6 +8,3 @@ assertUsage(
   !isBrowser(),
   "The file node_modules/react-streaming/dist/{esm,cjs}/server/hooks.js is loaded in the browser but it shouldn't. Make sure to never `import { something } from 'react-streaming/server'` in code that runs on the client-side. Also make sure your bundler picks the right node_modules/react-streaming/package.json#exports entries.",
 )
-
-import { useAsync } from './useAsync'
-import { useStream } from './useStream'
