c7abacccea4419b7bedff17d6d7e36c95f4103e0
Romuald Brillout <git@brillout.com>
2026-01-16 16:45:34 +0100

    Revert "play with React use()"
    This reverts commit 6c5f183af45588cd9df89536cb11f3ea5433abfe.

--- examples/vike-react/pages/streaming/+Page.tsx
+++ examples/vike-react/pages/streaming/+Page.tsx
@@ -1,6 +1,6 @@
 export default Page
 
-import React, { Suspense, use } from 'react'
+import React, { Suspense } from 'react'
 import { useAsync } from 'react-streaming'
 import { Counter } from '../../components/Counter'
 
@@ -13,6 +13,7 @@ function Page() {
         <a href="https://vike.dev/streaming#progressive-rendering">Progressive Rendering</a>. (Note how the interactive
         counter works before the data finished loading.)
       </p>
+      <Counter />
       <Suspense fallback={<p>Loading...</p>}>
         <MovieList />
       </Suspense>
@@ -21,44 +22,22 @@ function Page() {
 }
 
 function MovieList() {
-  console.log('MovieList')
-
-  //*
   const movies = useAsync(['star-wars-movies'], async () => {
-    console.log('FETCHING')
     const response = await fetch('https://star-wars.brillout.com/api/films.json')
     // Simulate slow network
     await new Promise((r) => setTimeout(r, 3 * 1000))
     const movies: Movie[] = (await response.json()).results
     return movies
   })
-  /*/
-  const movies = use((async () => {
-    console.log('FETCHING')
-    const response = await fetch('https://star-wars.brillout.com/api/films.json')
-    // Simulate slow network
-    await new Promise((r) => setTimeout(r, 3 * 1000))
-    const movies: Movie[] = (await response.json()).results
-    return movies
-  })())
-  //*/
-
-  console.log('movies', movies.length)
-
-  const rid = use(Promise.resolve(Math.random()))
-  console.log('rid', rid)
 
   return (
-    <>
-      <Counter />
-      <ol>
-        {movies.map((movies, index) => (
-          <li key={index}>
-            {movies.title} ({movies.release_date})
-          </li>
-        ))}
-      </ol>
-    </>
+    <ol>
+      {movies.map((movies, index) => (
+        <li key={index}>
+          {movies.title} ({movies.release_date})
+        </li>
+      ))}
+    </ol>
   )
 }
 
